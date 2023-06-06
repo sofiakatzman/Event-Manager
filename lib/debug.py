1b1163a32399765f5132169edfcd55ce324489cb
Sofia Katzman <katzmansof@gmail.com>
2023-06-06 18:45:44 -0400

    added connection to sqlite db & debugger

---
+++ lib/debug.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from models import (Locker, Instrument, Student)
+
+if __name__ == '__main__':
+    engine = create_engine("sqlite:///event_manager.db")
+    session = Session(engine, future=True)
+
+
+    import ipdb; ipdb.set_trace()
