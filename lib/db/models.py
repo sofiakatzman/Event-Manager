eaa9a1a2a00983e4222a846fb171f488cc7d34f0
Sofia Katzman <katzmansof@gmail.com>
2023-06-06 18:21:24 -0400

    added table creation logic

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,12 +1,38 @@
-# this is where my classes will be: 
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String())
+    last_name = Column(String())
+    position_id = Column(Integer())
+
+class Events(Base):
+    __tablename__ = 'events'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    date = Column(Integer())
+    description = Column(String())
+    tip_total = Column(Integer())
+    is_active = Column(String())
 
 
-class Users():
-    pass
+class Positions(Base):
+    __tablename__ = 'positions'
 
-class Events():
-    pass
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    tipout_percent = Column(Integer())
 
-class Positions():
-    pass
+
+# Current Tasks: (no logic yet)
+# step 1: set up classes -> tables 
+# step 2: set up class methods 
+# step 3: set up instance methods 
+
 
