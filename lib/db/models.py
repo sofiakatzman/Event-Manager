f56f52aa325f9a01e6007f9ccee72c6e907c279c
Sofia Katzman <katzmansof@gmail.com>
2023-06-07 14:49:44 -0400

    reset ttables to add foreign key values

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -9,7 +9,7 @@ class Users(Base):
     id = Column(Integer(), primary_key=True)
     first_name = Column(String())
     last_name = Column(String())
-    position_id = Column(Integer())
+    position_id = Column(Integer(), ForeignKey('positions.id'))  # this is a foreign key
 
     def __init__(self, first_name, last_name, position_id = 0):
         self.first_name = first_name
@@ -21,16 +21,6 @@ class Users(Base):
             Name: {self.first_name} {self.last_name}
             Position: {self.position_id}
         '''  
-
-    # def add_user():
-    #     pass
-
-    # def edit_user():
-    #     pass
-
-    # @classmethod
-    # def view_all(cls):
-    #     pass
 
 class Events(Base):
     __tablename__ = 'events'
@@ -55,12 +45,117 @@ class Events(Base):
             Event Description: {self.description}
             Event Date: {self.date}
             Event is Active ? : {self.is_active}
+            Event Tip Total : {self.tip_total}
             Event Staff: TBD  
         '''
          
     def close_out_event(self):
         pass
 
+
+class Positions(Base):
+    __tablename__ = 'positions'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    tipout_percent = Column(Integer())
+
+    def __init__(self, name):
+        self.name = name
+        self.tipout_percent = 0
+
+
+    def __repr__(self):
+        return f''' 
+            Position Name : {self.name}
+            Position Tipout Percentage : {self.tipout_percent}%
+        '''
+    
+
+class Tips(Base):
+    __tablename__ = 'tips'
+
+    id = Column(Integer(), primary_key=True)
+    event_id = Column(Integer(), ForeignKey("events.id")) # this is a foreign key 
+    user_id = Column(Integer(), ForeignKey("users.id")) # this is a foreign key
+    tipout_amount = Column(Integer())
+    check_number = Column(Integer()) 
+
+    def __init__(self, event_id, user_id):
+        self.event_id = event_id  
+        self.user_id = user_id  
+        self.tipout_amount = 0
+        self.check_number = "NOT PAID"
+
+    def __repr__(self):
+        return f''' 
+                Event ID : {self.event_id}
+                User ID : {self.user_id}
+                Tipout Amount : {self.tipout_amount}
+                Check Number : {self.check_number}
+            ''' 
+        
+
+class Schedules(Base):
+    __tablename__ = 'schedules'
+
+    id = Column(Integer(), primary_key=True)
+    event_id = Column(Integer(), ForeignKey("events.id")) # this is a foreign key 
+    event_type = Column(String())
+    user_id = Column(Integer(), ForeignKey("users.id")) # this is a foreign key 
+    position_id = Column(Integer(), ForeignKey("positions.id")) # this is a foreign key
+    arrival_time = Column(Integer())
+
+    def __init__(self, event_id, event_type, user_id, position_id, arrival_time):
+        self.event_id = event_id  
+        self.event_type = event_type
+        self.user_id = user_id   
+        self.position_id = position_id
+        self.arrival_time = arrival_time
+
+    def __repr__(self):
+        return f''' 
+                Event ID : {self.event_id}
+                Event Type : {self.event_type}
+                User ID : {self.user_id}
+                Position : {self.position_id}
+                Arrival Time : {self.arrival_time}
+        '''
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Current Tasks: (no logic yet)
+# ✔ 
+# ✔ add schemas for tips and schedule 
+# ✔ add repr methods for each Class
+# work on many-to-many relationships / associations on my tips and schedule tables
+# seed tips, schedule, and events tables
+# --> at this point I can begin building out the CLI component with my data 
+#  
+
+# Removed Code: 
+# Users
+    # def add_user():
+    #     pass
+
+    # def edit_user():
+    #     pass
+
+    # @classmethod
+    # def view_all(cls):
+    #     pass
+# Events
     # def create_event_schedule(self):
     #     pass
 
@@ -81,24 +176,7 @@ class Events(Base):
     # @classmethod
     # def view_all_closed(cls):
     #     pass
-
-class Positions(Base):
-    __tablename__ = 'positions'
-
-    id = Column(Integer(), primary_key=True)
-    name = Column(String())
-    tipout_percent = Column(Integer())
-
-    def __init__(self, name):
-        self.name = name
-        self.tipout_percent = 0
-
-    def __repr__(self):
-        return f''' 
-            Position Name : {self.name}
-            Position Tipout Percentage : {self.tipout_percent}%
-        '''
-
+# Positions
     # def edit_tipout(self):
     #     pass
 
@@ -114,74 +192,3 @@ class Positions(Base):
     #     pass
 
 
-
-
-
-class Tips(Base):
-    __tablename__ = 'tips'
-
-    id = Column(Integer(), primary_key=True)
-    event_id = Column(Integer()) # this is a foreign key / proxy association 
-    user_id = Column(Integer()) # this is a foreign key / proxy association 
-    tipout_amount = Column(Integer())
-    check_number = Column(Integer()) 
-
-    def __init__(self, event_id, user_id, tipout_amount, check_number="NOT PAID"):
-        self.event_id = event_id  # this is a foreign key / proxy association 
-        self.user_id = user_id  # this is a foreign key / proxy association 
-        self.tipout_amount = tipout_amount
-        self.check_number = check_number
-
-    def __repr__(self):
-        return f''' 
-                Event ID : {self.event_id}
-                User ID : {self.user_id}
-                Tipout Amount : {self.tipout_amount}
-                Check Number : {self.check_number}
-            ''' 
-        
-
-class Schedules(Base):
-    __tablename__ = 'schedules'
-
-    id = Column(Integer(), primary_key=True)
-    event_id = Column(Integer()) # this is a foreign key / proxy association 
-    event_type = Column(String())
-    user_id = Column(Integer()) # this is a foreign key / proxy association 
-    position_id = Column(Integer()) # this is a foreign key / proxy association 
-    arrival_time = Column(Integer())
-
-    def __init__(self, event_id, event_type, user_id, position_id, arrival_time):
-        self.event_id = event_id  # this is a foreign key / proxy association 
-        self.event_type = event_type
-        self.user_id = user_id  # this is a foreign key / proxy association 
-        self.position_id = position_id
-        self.arrival_time = arrival_time
-
-    def __repr__(self):
-        return f''' 
-                Event ID : {self.event_id}
-                Event Type : {self.event_type}
-                User ID : {self.user_id}
-                Position : {self.position_id}
-                Arrival Time : {self.arrival_time}
-        '''
-        # return f'{self.event_id, self.event_type, self.user_id, self.position_id, self.arrival_time}'
-        
-              
-        
-        
-        
-        
-# Current Tasks: (no logic yet)
-# ✔ 
-# ✔ add schemas for tips and schedule 
-# ✔ add print methods for each Class
-# work on many-to-many relationships / associations on my tips and schedule tables
-# seed tips, schedule, and events tables 
-
-
-
-
-
-# come back to whether or not i need to create classes for tips and schedule (i think i do)
