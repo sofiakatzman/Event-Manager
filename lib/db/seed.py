855a9cc32d205325fb0e8d52a0777d566e81ac45
Sofia Katzman <katzmansof@gmail.com>
2023-06-08 16:23:45 -0400

    adjusted comments & data structure positioning to reflect current state

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -18,15 +18,6 @@ session.query(Events).delete()
 session.query(Tips).delete()
 session.query(Schedules).delete()
 
-positions_list = {
-    0 : "Standby",
-    1 : "Waitress" , 
-    2 : "Bartender",
-    3 : "Busser",
-    4 : "Hostess",
-    5 : "Administration"
-    }
-
 # generate users seeded data
 users = []
 for i in range(30):
@@ -37,8 +28,16 @@ for i in range(30):
     )
     users.append(user)
 
-
 # generate position data
+positions_list = {
+    0 : "Standby",
+    1 : "Waitress" , 
+    2 : "Bartender",
+    3 : "Busser",
+    4 : "Hostess",
+    5 : "Administration"
+    }
+
 positions = []
 for position_id, position_name in positions_list.items():
     position = Positions(
@@ -48,16 +47,12 @@ for position_id, position_name in positions_list.items():
     positions.append(position)
 
 # generate event data
-#=> NO EVENT DATA FOR NOW ** 
-
 event_types = {"Art Gallery Opening" : "Art Gallery Opening for P&W" ,
                 "Private Event" : "Private Event for Sir Elton John",
                 "Music Show" : "Music Event at The Edge",
                 "Talent Showcase" : "Talent Showcase at Sony Hall", 
                 "Music Festival" : "CircoLoco Music Festival"}
 events = []
-
-# self, type, description, date
 start_date = date(2022, 1, 1)
 end_date = date(2023, 6, 1)
 for event_type, event_description in event_types.items():
@@ -69,15 +64,20 @@ for event_type, event_description in event_types.items():
     events.append(event)
 
 # Generate schedule data
+# no schedule data yet
+
+# def __init__(self, event_id, event_type, user_id, position_id, arrival_time=0000):
+schedules = []
+schedule1 = Schedules(1,"Art Gallery Opening", 1, 5, "12:00")
+schedules.append(schedule1)
 
 # Generate tip data
+tips = []
 
-# Seed data
+# no schedule data yet
+
 print("Seeding data...")
-
 # Add all data to the session
-session.add_all(users)
-session.add_all(positions)
-session.add_all(events)
-
+all_objects = users + positions + events + tips + schedules
+session.add_all(all_objects)
 session.commit()
