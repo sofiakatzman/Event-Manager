b69cf756aeaad5596f07af454ff174e8eb28f836
Sofia Katzman <katzmansof@gmail.com>
2023-06-09 19:22:50 -0400

    removed unnecessary __init__ from schemas

--- lib/helper_functions/tip_functions.py
+++ lib/helper_functions/tip_functions.py
@@ -1,6 +1,12 @@
 # Tips:
 #     1 - CALCULATE STAFF TIP PAYOUT BY EVENT - tipout()
 #     2 - PAY OUT STAFF - payout()
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from db.models import (Tips)
+
+engine = create_engine("sqlite:///db/event_manager.db")
+session = Session(engine, future=True)
 
 def test():
     print("Testing passed for Tips Module!")
