87c81c9f620f80a68f30104b66579f6d2543aef7
Sofia Katzman <katzmansof@gmail.com>
2023-06-09 22:45:39 -0400

    update user path to reflect functionality adjustments made

--- lib/cli.py
+++ lib/cli.py
@@ -33,9 +33,9 @@ def reroute():
             main()
 
 def main():
-    # enter main menu choice options
+#=> enter main menu choice options
     choice = 0
-    while choice != 6:
+    while choice != 5:
         print("***EVENT MANAGER***")
         print("Hello...")
         print('''
@@ -43,14 +43,13 @@ def main():
                     1 - USERS 
                     2 - POSITIONS
                     3 - EVENTS
-                    5 - REPORTS
-                    6 - EXIT
+                    4 - REPORTS
+                    5 - EXIT
         ''')
 
         choice = int(input()) 
 
-        # enter staffs module (1)
-        
+#=> 1 - enter staffs module
         if choice == 1:
             staff_choice = 0  
             while staff_choice != 5:
@@ -82,8 +81,7 @@ def main():
                     staff_functions.delete()
                     reroute()
 
-        ## enter positions module (2)
-        
+##+> 2 - enter positions module      
         if choice == 2:
             position_choice = 0
             while position_choice != 5:
@@ -115,8 +113,7 @@ def main():
                     position_functions.delete()
                     reroute()
 
-        ## enter events module (3)        
-        
+##=> 3 - enter events module     
         if choice == 3:
             events_choice = 0
             while events_choice != 6:
@@ -153,10 +150,9 @@ def main():
                     event_functions.view() 
                     reroute()
                             
-
-        ## enter reporting module (5)
+##=> 4-  enter reporting module
         reports_choice = 0
-        if choice == 5:
+        if choice == 4:
             while reports_choice != 3:
                 print("Entering Reporting Module...")
                 print('''
@@ -180,28 +176,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# Helper Functions By Module Key: location = /lib/helper_functions
-# Staff Module (Main menu choice = 1): staff_functions.py
-# 1 - VIEW USERS - view()
-# 2 - ADD A USER - add()
-# 3 - EDIT A USER - edit()
-# 4 - DELETE A USER - delete()
-
-# Position Module (Main menu choice = 2): position_functions.py
-# 1 - VIEW POSITIONS - view()
-# 2 - ADD A POSITION - add()
-# 3 - EDIT POSITION TIP OUT PERCENTAGE - edit()
-# 4 - DELETE A POSITION - delete()
-
-# Event Module (Main menu choice = 3): event_functions.py 
-# 1 - CREATE AN EVENT - add()
-# 2 - CREATE AN EVENT STAFF SCHEDULE - create_schedule()
-# 3 - EDIT AN EVENT STAFF SCHEDULE - edit_schedule()
-# 4 - CLOSE OUT AN EVENT - closeout()
-# 5 - VIEW EVENT HISTORY - view()
-
-
-# Reporting Module (Main menu choice = 5): reporting_functions.py
-# 1 - VIEW OPEN EVENTS - view_open_events()
-# 2 - VIEW STAFF BY POSITIONS - staff_by_position()
